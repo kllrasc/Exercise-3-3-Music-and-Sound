eab3e2e8dd87dcd43690750f26c3a51b34198ed2
kllrasc <kllrasc@iu.edu>
2023-10-05 01:04:21 -0400

    Completes Assignment

--- Paddle/Paddle.gd
+++ Paddle/Paddle.gd
@@ -28,3 +28,7 @@ func hit(_ball):
 	tween.tween_property($Images/Highlight, "modulate:a", 0.0, time_highlight)
 	$Images/Highlight.scale = Vector2(2.0,2.0)
 	tween.tween_property($Images/Highlight, "scale", Vector2(1.0,1.0), time_highlight_size).set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_IN)
+	$ColorRect.color = Color8(201,42,42,255)
+	var paddle_sound = get_node_or_null("/root/Game/Paddle_Sound")
+	if paddle_sound != null:
+		paddle_sound.play()
